e5ca94efa9e3f851e87735f8dea48d012847f864
h4vokh <havokh@live.com>
2015-05-16 22:29:30 +0800

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 32
+
+template1 = ERB.new "The value of x and y is: <%= y%> & <%= y %>"
+puts template1.result(binding)
+
+template3 = ERB.new "The sum of x and y is: <%=x + y %>" 
+puts template3.result(binding)
